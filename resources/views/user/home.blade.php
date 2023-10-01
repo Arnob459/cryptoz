c65334e221fca01f3ac78c998ea65a98aea23537
Mahin zaman Arnob <arnobmahin007@gmail.com>
2023-10-01 19:59:44 +0600

    user home

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,7 +1,218 @@
 
+@extends('user.layouts.master')
 
-helllloooooooo
-<a href="{{ route('user.logout') }}" class="menu-link">
-    <i class="menu-icon tf-icons bx bx-logout-circle"></i>
-    <div data-i18n="Analytics">logout</div>
-  </a>
+@section('user')
+
+
+
+                    <div class="dashboard-hero-content text-white">
+                        <h3 class="title">Dashboard</h3>
+                        <ul class="breadcrumb">
+                            <li>
+                                <a href="">Home</a>
+                            </li>
+                            <li>
+                                Dashboard
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="container-fluid">
+                    <div class="row justify-content-center mt--85">
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Deposit wallet Balance </span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Interest Wallet Balance </span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Total Deposit</span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Total Withdrawal</span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Total Investment </span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Total Interest return </span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Today Interest return </span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Yesterday Interest return</span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Last 7 days Interest return </span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Last 30 days Interest return </span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Total Referral Commission </span>
+                                        <h5 class="amount">0 USD</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <img src="{{ asset('assets/user/images/dashboard/dashboard1.png') }}" alt="dasboard">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="dashboard-item">
+                                <div class="dashboard-inner">
+                                    <div class="col-md-9">
+                                        <span class="title">Total Referral</span>
+                                        <h5 class="amount">0 </h5>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="container-fluid sticky-bottom">
+                    <div class="footer-bottom">
+                        <div class="footer-bottom-area">
+                            <div class="left">
+                                <p>&copy; 2020 <a href="#0">Hyipland</a> | All right reserved</p>
+                            </div>
+                            <ul class="social-icons">
+                                <li>
+                                    <a href="#0">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#0" class="active">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#0">
+                                        <i class="fab fa-pinterest-p"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#0">
+                                        <i class="fab fa-instagram"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!--=======SideHeader-Section Ends Here=======-->
+
+
+    </div>
+
+@endsection
---
+++ resources/views/user/layouts/master.blade.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from pixner.net/hyipland/demo/dashboard.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Sep 2023 12:10:42 GMT -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>{{ $gnl->site_name }}</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/user/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/user/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/user/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/user/css/odometer.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/user/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/user/css/owl.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/user/css/jquery-ui.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/user/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/user/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/user/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/admin/fontawesome-5.15.4/css/all.min.css') }}">
+
+
+    <link rel="shortcut icon" href="{{ asset('assets/admin/images/logo/favicon.png') }}" type="image/x-icon">
+</head>
+
+<body>
+    <div class="main--body dashboard-bg">
+        <!--========== Preloader ==========-->
+        <div class="loader">
+            <div class="loader-inner">
+                <div class="loader-line-wrap">
+                    <div class="loader-line"></div>
+                </div>
+                <div class="loader-line-wrap">
+                    <div class="loader-line"></div>
+                </div>
+                <div class="loader-line-wrap">
+                    <div class="loader-line"></div>
+                </div>
+                <div class="loader-line-wrap">
+                    <div class="loader-line"></div>
+                </div>
+                <div class="loader-line-wrap">
+                    <div class="loader-line"></div>
+                </div>
+            </div>
+        </div>
+        <div class="overlay"></div>
+        <!--========== Preloader ==========-->
+
+
+        <!--=======SideHeader-Section Starts Here=======-->
+        <div class="notify-overlay"></div>
+        <section class="dashboard-section">
+            <div class="side-header oh">
+                <div class="cross-header-bar d-xl-none">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="site-header-container">
+                    <div class="side-logo">
+                        <a href="dashboard.html">
+                            <img src="{{ asset('assets/admin/images/logo/logo.png') }}" alt="logo">
+                        </a>
+                    </div>
+                    <ul class="dashboard-menu">
+                        <li>
+                            <a href="dashboard.html" class="active"><i class="flaticon-man"></i>Dashboard</a>
+                        </li>
+
+                        <li>
+                            <a href=""><i class="flaticon-interest"></i>Investment Plan</a>
+                        </li>
+
+                        <li>
+                            <a href="fund-transfer.html"><i class="fas fa-book"></i>Return Log </a>
+                        </li>
+                        <li>
+                            <a href="deposit.html"><i class="fas fa-coins"></i>Deposit Now</a>
+                        </li>
+                        <li>
+                            <a href="deposit.html"><i class="flaticon-exchange"></i>Deposit History</a>
+                        </li>
+                        <li>
+                            <a href="withdraw.html"><i class="flaticon-atm"></i>Withdraw</a>
+                        </li>
+                        <li>
+                            <a href="partners.html"><i class="flaticon-exchange"></i>Withdraw History</a>
+                        </li>
+
+                        <li>
+                            <a href="ticket.html"><i class="flaticon-deal"></i>Transactions</a>
+                        </li>
+                        <li>
+                            <a href="promotional-metarials.html"><i class="fas fa-users"></i>Referral Statistic</a>
+                        </li>
+                        <li>
+                            <a href="promotional-metarials.html"><i class="fas fa-coins"></i>Referral Commissions</a>
+                        </li>
+                        <li>
+                            <a href="promotional-metarials.html"><i class="fas fa-user"></i>Profile </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('user.logout') }}"><i class="flaticon-right-arrow"></i>Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <div class="dasboard-body">
+                <div class="dashboard-hero">
+
+
+                        <div class="header-bottom">
+                            <div class="container">
+                                <div class="header-area">
+                                    <div class="logo">
+                                        <a href="index.html">
+                                            <img src="{{ asset('assets/admin/images/logo/logo.png') }}" alt="logo">
+                                        </a>
+                                    </div>
+                                    <ul class="menu">
+
+                                        <li>
+                                            <a href="">Dashboard</a>
+                                        </li>
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                                               aria-haspopup="true" aria-expanded="false">Investment</a>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href=" /investment-plans">Investment plans</a></li>
+                                                <li><a class="dropdown-item" href=" /invest-return">Investment return</a></li>
+                                                <li><a class="dropdown-item" href=" /invest-return/history">Investment return history</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                                               aria-haspopup="true" aria-expanded="false">Deposit</a>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href=" /deposit">Deposit now</a></li>
+                                                <li><a class="dropdown-item" href=" /deposit/history">Deposit history</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                                               aria-haspopup="true" aria-expanded="false">Withdraw</a>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href=" /withdraw">Withdraw now</a></li>
+                                                <li><a class="dropdown-item" href=" /withdraw/history">Withdraw history</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                                               aria-haspopup="true" aria-expanded="false">Referral</a>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href=" /referral-statistic">Referral statistic</a></li>
+                                                <li><a class="dropdown-item" href=" /referral-commission">Referral commissions</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+
+                                        <li class="nav-item"><a class="nav-link" href="">Transactions</a></li>
+
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                                               aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+                                            <ul class="dropdown-menu last">
+                                                <li><a class="dropdown-item" href=" /profile">Profile</a></li>
+                                                <li><a class="dropdown-item" href=" /change-password">Change Password</a></li>
+                                                <li><a class="dropdown-item" href=" /support">Support</a></li>
+                                                <li><a class="dropdown-item" href=" /g2fa">2Fa Security</a></li>
+                                                <li><a class="dropdown-item" href="{{ route('user.logout') }}">Logout</a></li>
+
+                                            </ul>
+                                        </li>
+
+                                    </ul>
+                                    <div class="header-bar d-lg-none">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @yield('user')
+                        <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="{{ asset('assets/user/js/jquery-3.3.1.min.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/modernizr-3.6.0.min.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/plugins.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/bootstrap.min.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/magnific-popup.min.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/jquery-ui.min.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/wow.min.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/odometer.min.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/viewport.jquery.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/nice-select.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/owl.min.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/paroller.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/chart.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/circle-progress.js') }}"></script>
+                        <script src="{{ asset('assets/user/js/main.js') }}"></script>
+
+
+
+                    </body>
+
+
+                    <!-- Mirrored from pixner.net/hyipland/demo/dashboard.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Sep 2023 12:11:00 GMT -->
+                    </html>
